970e176fd34d02ff57bb534cff7c2f432bbc0409
jypara-git <zhyparaabdieva@gmail.com>
2021-03-27 09:17:26 -0700

    create an HTML template

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
+const [name, github] = profileDataArgs;
+// const printProfileData = profileDataArr => {
+//     // This ...
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
+//     console.log('==========');
 
-const printProfileData = profileDataArr => {
-    // This ...
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('==========');
+//     // Is the same as this...
+//     profileDataArr.forEach((profileItem => console.log(profileItem))
+//     )}
+// printProfileData(profileDataArgs);
 
-    // Is the same as this...
-    profileDataArr.forEach((profileItem => console.log(profileItem))
-    )}
-printProfileData(profileDataArgs);
+const generatePage = (name, github) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
+  
+    <body>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
+};
+console.log(name, github);
+console.log(generatePage(name, github));
